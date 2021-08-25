001e19fb048305a258b1e016f4644303d0a64d61
Dimas Nurfauzi <dimasnrfzi@gmail.com>
2021-08-25 17:26:19 +0700

    implement password hashing in signup using bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,13 +2,18 @@ import { ConflictException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentials: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentials;
 
-    const user = this.create({ username, password });
+    // hashing password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
