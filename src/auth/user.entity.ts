6076013b4eff71096649557b73f39343672291c8
Dimas Nurfauzi <dimasnrfzi@gmail.com>
2021-08-25 21:07:18 +0700

    hide user password field

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import { Task } from 'src/tasks/task.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
@@ -10,6 +11,7 @@ export class User {
   username: string;
 
   @Column()
+  @Exclude({ toPlainOnly: true })
   password: string;
 
   @OneToMany(() => Task, (task) => task.user, { eager: true })
