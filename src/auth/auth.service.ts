d00b257d02f492e3eb7b53136467ff0f3a83c730
Dimas Nurfauzi <dimasnrfzi@gmail.com>
2021-08-25 18:29:36 +0700

    create simple auth sign in function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,20 @@ export class AuthService {
 
   signUp(authCredentials: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentials);
+  }
+
+  async signIn(authCredentials: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentials;
+
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user) {
+      const isCorrectPassword = await bcrypt.compare(password, user.password);
+      if (isCorrectPassword) {
+        return 'success';
+      }
+    }
+
+    throw new UnauthorizedException('Username or password mismatch');
   }
 }
