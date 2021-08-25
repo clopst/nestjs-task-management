ee8764c1d48494f907f5db57e213aaa9c2b58797
Dimas Nurfauzi <dimasnrfzi@gmail.com>
2021-08-25 17:20:32 +0700

    create simple auth signup function

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(128)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak',
+  }) // https://gist.github.com/arielweinberger/18a29bfa17072444d45adaeeb8e92ddc
+  password: string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,22 @@
+import { ConflictException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentials: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentials;
+
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+      throw error;
+    }
+  }
+}
